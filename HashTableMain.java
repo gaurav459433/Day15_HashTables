8ff3a1b23065aa3f6f1d259370e2406d7f21e5ed
gaurav459433 <gaurav459433@gmail.com>
2023-03-20 18:01:49 +0530

    Welcome To Data Structure Program Using Java Generics: Hash Tables

---
+++ HashTableMain.java
@@ -0,0 +1,8 @@
+package assignments259.dshashtable;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Program Using Java Generics: Hash Tables");
+    }
+
+}
