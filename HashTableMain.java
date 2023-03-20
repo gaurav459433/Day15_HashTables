4e968ba7dce5e97de5739612580948f608ad685a
gaurav459433 <gaurav459433@gmail.com>
2023-03-20 18:14:01 +0530

    Added: Code For Find Frequency Of Words In a Sentence

--- HashTableMain.java
+++ HashTableMain.java
@@ -3,6 +3,19 @@ package assignments259.dshashtable;
 public class HashTableMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Data Structure Program Using Java Generics: Hash Tables");
-    }
+        HashTableDS<String, Integer> hashTable = new HashTableDS<>();
+        String sentence = "To be or not to be";
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
 
+        for (String word : sentenceArray) {
+            Integer count = hashTable.get(word);
+
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashTable.add(word, count);
+        }
+        System.out.println(hashTable);
+    }
 }
